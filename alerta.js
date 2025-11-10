29e1e0c937292225e5bdd039f6ea2367a1b8e0b5
Francisco Ariel Lombardo Medina <arielxh65@gmail.com>
2025-11-10 18:51:13 -0300

    Update alerta.js

--- alerta.js
+++ alerta.js
@@ -1,3 +1,4 @@
-alert("Bienvenido a la prueba JavaScript");
-
-document.querySelector("h1").textContent = "¡ESTO INDICA QUE FUNCIONA JAVASCRIPT!";
+alert("¡Bienvenido a la prueba JavaScript!"); // (a)
+let userName = prompt("Para personalizar tu experiencia, por favor, ingresa tu nombre:"); // (b)
+let welcomeText = `¡Bienvenido/a de nuevo, ${userName}!`;
+alert(welcomeText); //
