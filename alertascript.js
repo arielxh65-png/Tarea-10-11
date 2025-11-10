a2093f1cbc1812ed0452a704ca52f1ca3b72946d
Francisco Ariel Lombardo Medina <arielxh65@gmail.com>
2025-11-10 18:57:13 -0300

    Delete alertascript.js

--- alertascript.js
+++
@@ -1,4 +0,0 @@
-alert("¡Bienvenido a la prueba JavaScript!"); // (a)
-let userName = prompt("Para personalizar tu experiencia, por favor, ingresa tu nombre:"); // (b)
-let welcomeText = `¡Bienvenido/a de nuevo, ${userName}!`;
-alert(welcomeText); //
